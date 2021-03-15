2ea596943061dd79082d2c50d870a3dfe5c4161e
Francis Brunet-Manquat <francis.brunet-manquat@univ-grenoble-alpes.fr>
2021-03-15 13:27:30 +0100

    -

--- src/Search/AdherentSearch.php
+++ src/Search/AdherentSearch.php
@@ -3,8 +3,42 @@
 
 namespace App\Search;
 
+use App\Entity\Adherent;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AdherentSearch
-{
+class AdherentSearch extends AbstractController {
+
+    public function identifiantNormalise(Adherent $adherent) {
+        $nom =  $adherent->getNom();
+        $prenom =  $adherent->getPrenom();
+        $date = $adherent->getDateNaissance();
+        preg_replace("/[^A-Za-z]/", '', [$prenom, $nom]);
+
+        $nom =  iconv('UTF-8','ASCII//TRANSLIT',$nom);
+        $prenom =  iconv('UTF-8','ASCII//TRANSLIT',$prenom);
+
+        $ident =  $date->format("dmY") . strtoupper($nom) . '_' . strtoupper($prenom);
+
+        return $ident;
+    }
+
+    public function  rechercheNom($nom, $price) {
+
+
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT p
+            FROM App\Entity\Product p
+            WHERE p.price > :price
+            ORDER BY p.price ASC'
+        )->setParameter('price', $price);
+
+        // returns an array of Product objects
+        return $query->getResult();
+
+    }
+
 
 }
