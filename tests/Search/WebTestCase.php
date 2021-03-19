68a361fe1ce7c4b69da39450ec183dacd425e353
Teo Berguerre <teo.berguerre@etu.univ-grenoble-alpes.fr>
2021-03-19 09:49:15 +0100

    test a Response 200 /inscription FAIL

--- tests/Search/WebTestCase.php
+++ tests/Search/WebTestCase.php
@@ -20,11 +20,11 @@ class WebTestCase extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
     /*
     * On test que la page existe
     */
-    /*public function testAnonymePageExiste(){
+    public function testAnonymePageExiste(){
         $client = static::createClient();
         $client->request('GET','/');
         $this->assertEquals(200,$client->getResponse()->getStatusCode());
-    }*/
+    }
 
     /*
      * On test que ka page anonyme existe et qu'il y a un lien vers inscription
@@ -37,6 +37,12 @@ class WebTestCase extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
         $uri = $link->getUri();
         $this->assertIsString($uri,'url est bien un string');
         $this->assertEquals('http://localhost/inscription', $uri,'On a bien un url pour la page inscription');
+    }
+
+    public function testResponsetoInscriptionPage(){
+        $client = static::createClient();
+        $client->request('GET','/inscription');
+        $this->assertResponseIsSuccessful();
     }
 
 }
