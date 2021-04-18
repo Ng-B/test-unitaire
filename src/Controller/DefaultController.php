9bad2839f96d0d49b55996dfac88d58bab8d62e5
Ng-B <naodg68@gmail.com>
2021-04-18 18:07:51 +0200

    Trajets + SQL

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Trajet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Security;
 
@@ -12,7 +13,6 @@ class DefaultController extends AbstractController
 
     public function index(Security $security) {
 
-        $communes = json_decode(file_get_contents('/home/ngB/Documents/LP-AW/Test_Unitaire/test-unitaire/public/json/france.json'));
 
         if ($security->getUser()) {
             return $this->render('accueil.html.twig', ['user' => $security->getUser()]);
@@ -22,6 +22,14 @@ class DefaultController extends AbstractController
 
     public function login() {
         return $this->render('login.html.twig', []);
+    }
+
+    public function trajets() {
+        $trajets = $this->getDoctrine()
+            ->getRepository(Trajet::class)
+            ->findAll();
+
+        return $this->render('trajets.html.twig', ['trajets' => $trajets]);
     }
 
     public function inscription() {
